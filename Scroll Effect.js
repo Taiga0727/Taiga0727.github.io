b154124b5886e1fd2ce18228d642117c66bc97b6
Taiga0727 <atilak.work@gmail.com>
2024-12-20 15:57:38 +0700

    Add files via upload

---
+++ Scroll Effect.js
@@ -0,0 +1,9 @@
+// Add sticky class to navbar when scrolling
+window.onscroll = function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+};
